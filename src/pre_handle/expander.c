d9fc3e8e3b045630568a6a6a605f687c3e14246f
Jingjing Wu <jingwu@c2r6p2.hive.fi>
2024-09-30 10:43:31 +0300

    expander() done

--- src/pre_handle/expander.c
+++ src/pre_handle/expander.c
@@ -6,7 +6,7 @@
 /*   By: jingwu <jingwu@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 08:43:11 by jingwu            #+#    #+#             */
-/*   Updated: 2024/09/30 09:37:42 by jingwu           ###   ########.fr       */
+/*   Updated: 2024/09/30 10:42:31 by jingwu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,62 @@
 /*
 	The name doesn't not contain the '$' at the beginning.
 	For example:
-		1. $cat, will just return "cat" as the env_name;
+		1. $cat, will just return "$cat" as the env_name;
 */
 static char *get_env_name(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (str[0] == '?');
+	if(str[i] == '$')
+		i++;
+	if (str[i] == '?');
 		return (ft_strdup("$?"));
 	while (ft_isalnum(str[i]) || str[i] == '_')
 		i++;
 	return (ft_substr(str, 0 , i));
 }
+
+static char	*replace(char *str, char *name, char *value)
+{
+	int		len;
+	char	*newstr;
+	char	*tmp;
+	char	*bef_name;
+
+	if (!str | !name | !value)
+		return (ft_strdup(""));
+	len = ft_strlen(str) + ft_strlen(value) - ft_strlen(name);
+	newstr = ft_calloc((len + 1), sizeof(char));
+	if (!newstr)
+		return (ft_strdup(""));
+	tmp = ft_strnstr(str, name, (ft_strlen(str))); // find the start position of the name
+	bef_name = ft_substr(str, 0, (tmp - str));
+	ft_strlcat(newstr, bef_name, (len + 1));
+	ft_strlcat(newstr, value, (len + 1));
+	ft_strlcat(newstr, (tmp + ft_strlen(name)), len + 1);
+	free(bef_name);
+	return (newstr);
+}
+
 /*
-	This function will
+	This function will ge the env_value first, then call function
+	replace() to replace the env_name with env_value.
 
 */
-static void	add_to_env_list(char *env_name)
+static void	replace_env(t_token *token, char *env_name)
 {
 	char	*env_value;
+	int		len;
+	char	*new_str;
 
-	if (!env_name)
+	if (!token || !env_name)
 		return ;
-	if (!ft_strcmp(env_name, "?"))
+	if (!ft_strcmp(env_name, "$?"))
 		env_value = ft_itoa(ms() ->exit);
 	else
 		env_value = get_env_value(env_name);
-	
+	token->str = replace(token->str, env_name, env_value);
 }
 
 /*
@@ -54,7 +82,7 @@ static void	add_to_env_list(char *env_name)
 		3. echo "$name' abc"
 		4. echo 'abc $name out"put'
 */
-static void	expand(char *str)
+static void	expand(t_token *token)
 {
 	bool	sg_quote;
 	bool	db_quote;
@@ -64,17 +92,18 @@ static void	expand(char *str)
 	sg_quote = false;
 	db_quote = false;
 	i = 0;
-	while (str[i])
+	while (token->str[i])
 	{
 		j = 1;
-		if (str[i] == '\"' && !sg_quote && !db_quote)
+		if (token->str[i] == '\"' && !sg_quote && !db_quote)
 			db_quote = true;
-		else if (str[i] == '\'' && !db_quote && !sg_quote)
+		else if (token->str[i] == '\'' && !db_quote && !sg_quote)
 			sg_quote = true;
-		else if (str[i] == '$' && !is_seperator(str[i + 1]) && sg_quote)
+		else if (token->str[i] == '$' && !is_seperator(token->str[i + 1])
+			&& sg_quote)
 		{
-			add_to_env_list(get_env_name(&str[i + 1]));
-			j = ft_strlen(get_env_name(&str[i + 1]));
+			replace_env(token, get_env_name(&(token->str[i])));
+			j = ft_strlen(get_env_name(&(token->str[i])));
 		}
 		i += j;
 	}
@@ -92,7 +121,7 @@ void	expander(void)
 		if (token->tk_type == TK_WORD)
 		{
 			if (ft_strchr(token->str, '$'))
-				expand(token->str);// inside expand()it may do not expand.
+				expand(token);// inside expand()it may do not expand.
 		}
 		tk_list_manager(NEXT);
 	}
