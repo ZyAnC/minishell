0b94a0da8a2a6037d3c599221adcf85fd1a617d9
Yan Zheng <yzheng@c2r6p6.hive.fi>
2024-11-13 09:31:25 +0200

    final

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jingwu <jingwu@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: yzheng <yzheng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 11:00:09 by yzheng            #+#    #+#             */
-/*   Updated: 2024/11/12 13:57:36 by jingwu           ###   ########.fr       */
+/*   Updated: 2024/11/13 09:28:09 by yzheng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,7 +107,8 @@ void	real_execute(t_cmd *cm);
 void	exe(t_cmd *cm);
 int		ft_env(void);
 void	heredoc(t_cmd *cm);
-
+int		builtin(char **cmd);
+void	real_execute(t_cmd *cm);
 /*<------redirect------->*/
 int		set_errors(char *message);
 int		set_fd(t_cmd *cm);
--- src/execute/exe.c
+++ src/execute/exe.c
@@ -3,77 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   exe.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jingwu <jingwu@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: yzheng <yzheng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 13:53:13 by yzheng            #+#    #+#             */
-/*   Updated: 2024/11/12 14:17:08 by jingwu           ###   ########.fr       */
+/*   Updated: 2024/11/13 09:29:21 by yzheng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
 
-static inline void	ft_execve_failed(char **shellcmd, char *path)
+void	check_signal(int b)
 {
-	char	*message;
-
-	if (!access(shellcmd[0], F_OK) && !access(shellcmd[0], X_OK))
-	{
-		if (path)
-			ft_free_str(path);
-		if (ft_strchr(shellcmd[0], '/'))
-			ex_error(shellcmd[0], DIRECTORY, 126);
-		else
-			ex_error(shellcmd[0], COMMAND, 127);
-	}
-	message = ft_strjoin(shellcmd[0], " : ");
-	if (errno == 13)
-		ex_error(message, ERR, 126);
-	else if (errno == 2)
-		ex_error(message, ERR, 127);
-	ex_error(message, ERR, 1);
-	ft_free_str(message);
-	if (path)
-		ft_free_str(path);
-}
-
-static int	builtin(char **cmd)
-{
-	int	size;
-
-	size = ft_strlen(cmd[0]);
-	if (size == 3 && !ft_strncmp(cmd[0], "env", 3))
-		return (ft_env());
-	else if (size == 2 && !ft_strncmp(cmd[0], "cd", 2))
-		return (ft_cd(cmd));
-	else if (size == 4 && !ft_strncmp(cmd[0], "echo", 4))
-		return (ft_echo(cmd));
-	else if (size == 3 && !ft_strncmp(cmd[0], "pwd", 3))
-	{
-		ft_printf("%s\n", ms()->cwd);
-		return (1);
-	}
-	else if (size == 5 && !ft_strncmp(cmd[0], "unset", 5))
-		return (ft_unset(cmd));
-	else if (size == 6 && !ft_strncmp(cmd[0], "export", 6))
-		return (ft_export(cmd));
-	else if (size == 4 && !ft_strncmp(cmd[0], "exit", 4))
-		ft_exit(cmd);
-	return (0);
-}
-
-void	real_execute(t_cmd *cm)
-{
-	char	*path;
-
-	if (!builtin(cm->cmd))
-	{
-		path = findvalidcmd(cm->cmd);
-		if (!path)
-			ex_error(cm->cmd[0], COMMAND, 127);
-		execve(path, cm->cmd, ms()->env);
-		ft_execve_failed(cm->cmd, path);
-	}
-	exit(ms()->exit);
+	if (ms()->exit == 130 && b > 1)
+		printf("\n");
+	if (ms()->exit == 130 && b == 1)
+		restart(0);
 }
 
 static int	exe_heart(int i, t_cmd *cm, int *prev_fd, int b)
@@ -98,12 +42,34 @@ static int	exe_heart(int i, t_cmd *cm, int *prev_fd, int b)
 			exe_pipe3(cm);
 			close(ms()->fd[0]);
 		}
-		if (ms()->exit == 130 && b > 1)
-			printf("\n");
-		if (ms()->exit == 130 && b == 1)
-			restart(0);
+		check_signal(b);
 	}
 	return (1);
+}
+
+int	check_cat(t_cmd *cm, int i, int b, int prev_fd)
+{
+	i = set_fd(cm);
+	if (!i && (!ft_strncmp(cm->cmd[0], "cat", 3)
+			|| !ft_strncmp(cm->cmd[0], "grep", 4)))
+	{
+		cm = cm->next;
+		if (cm && !ft_strncmp(cm->cmd[0], "cat", 3)
+			&& cm->intype == TK_PIPE)
+		{
+			ms()->exit = 0;
+			return (0);
+		}
+		return (1);
+	}
+	if (!cm->cmd && cm->intype != TK_HDOC)
+		return (0);
+	if (b == 1 && cm->outype == TK_NONE && cm->intype != TK_HDOC)
+		if (ft_strncmp(cm->cmd[0], "echo", 4)
+			&& ft_strncmp(cm->cmd[0], "pwd", 3) && builtin(cm->cmd))
+			return (0);
+	exe_heart(i, cm, &prev_fd, b);
+	return (2);
 }
 
 void	exe(t_cmd *cm)
@@ -111,31 +77,18 @@ void	exe(t_cmd *cm)
 	int		prev_fd;
 	int		i;
 	int		b;
+	int		status;
 
+	i = 0;
 	prev_fd = -1;
 	b = count_cm(cm);
 	while (cm)
 	{
-		i = set_fd(cm);
-		if (!i && (!ft_strncmp(cm->cmd[0], "cat", 3)
-				|| !ft_strncmp(cm->cmd[0], "grep", 4)))
-		{
-			cm = cm->next;
-			if (cm && !ft_strncmp(cm->cmd[0], "cat", 3)
-				&& cm->intype == TK_PIPE)
-			{
-				ms()->exit = 0;
-				break ;
-			}
-			continue ;
-		}
-		if (!cm->cmd && cm->intype != TK_HDOC)
+		status = check_cat(cm, i, b, prev_fd);
+		if (status == 0)
 			break ;
-		if (b == 1 && cm->outype == TK_NONE && cm->intype != TK_HDOC)
-			if (ft_strncmp(cm->cmd[0], "echo", 4) && ft_strncmp(cm->cmd[0],
-					"pwd", 3) && builtin(cm->cmd))
-				break ;
-		exe_heart(i, cm, &prev_fd, b);
+		else if (status == 1)
+			continue ;
 		if ((ms()->exit == 130 && ms()->hstatus == 1))
 			break ;
 		cm = cm->next;
